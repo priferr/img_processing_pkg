90c0c56a16d4f70907ef2380ba97346d16b75e6a
Priscilla Cosi <priferr@gmail.com>
2024-10-06 16:35:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="img_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Priferr",
+    author_email="ipriferr@gmail.com",
+    description="Study Image Processing Using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/priferr/img_processing_pkg"
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
 )
